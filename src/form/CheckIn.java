7d97be72934db1a6d5ce3dc41aa27afaa537dd0f
neelkapadia <neel.kapadia10@gmail.com>
2018-04-12 07:53:49 -0400

    Room and Customer

--- src/form/CheckIn.java
+++ src/form/CheckIn.java
@@ -15,7 +15,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -28,6 +31,7 @@ public class CheckIn extends javax.swing.JFrame {
      */
     public CheckIn() {
         initComponents();
+        getRooms();
     }
 
     /**
@@ -45,11 +49,11 @@ public class CheckIn extends javax.swing.JFrame {
         Confrim = new javax.swing.JButton();
         Home = new javax.swing.JButton();
         Logout = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jList2 = new javax.swing.JList<>();
         jLabel3 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        Customer = new javax.swing.JTable();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        Room = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -79,59 +83,75 @@ public class CheckIn extends javax.swing.JFrame {
             }
         });
 
-        jScrollPane1.setViewportView(jList1);
-
-        jScrollPane2.setViewportView(jList2);
-
         jLabel3.setText("Select room:");
+
+        Customer.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Cust ID", "Name", "Email"
+            }
+        ));
+        jScrollPane3.setViewportView(Customer);
+
+        Room.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Room Num", "Category", "Capacity", "Price"
+            }
+        ));
+        Room.setUpdateSelectionOnSort(false);
+        jScrollPane4.setViewportView(Room);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(259, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(242, 242, 242))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(378, 378, 378)
+                        .addComponent(Confrim)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(Home)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(Logout))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(Confrim)
-                        .addGap(250, 250, 250))))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(80, 80, 80)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(37, 37, 37))
+                        .addGap(25, 25, 25)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 384, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 384, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(86, 86, 86))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(370, 370, 370)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(51, 51, 51)
+                .addContainerGap(50, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addGap(34, 34, 34)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGap(44, 44, 44)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(Confrim)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
+                .addGap(44, 44, 44)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Logout)
-                    .addComponent(Home)))
+                    .addComponent(Confrim)
+                    .addComponent(Home)
+                    .addComponent(Logout)))
         );
 
         pack();
@@ -140,15 +160,11 @@ public class CheckIn extends javax.swing.JFrame {
     private void ConfirmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConfirmActionPerformed
         // TODO add your handling code here:
         JFrame jf = new JFrame();
-        JOptionPane.showMessageDialog(jf,"BOOKING CONFIRMED","",JOptionPane.INFORMATION_MESSAGE);
-
-
+        JOptionPane.showMessageDialog(jf, "BOOKING CONFIRMED", "", JOptionPane.INFORMATION_MESSAGE);
         //commit rooms
         FrontDesk fd = new FrontDesk();
         sysExit();
         fd.setVisible(true);
-
-
     }//GEN-LAST:event_ConfirmActionPerformed
 
     private void HomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_HomeActionPerformed
@@ -165,36 +181,72 @@ public class CheckIn extends javax.swing.JFrame {
         l.setVisible(true);
     }//GEN-LAST:event_LogoutActionPerformed
 
-    public void sysExit(){
-        WindowEvent winClosing = new WindowEvent(this,WindowEvent.WINDOW_CLOSING);
+    public void sysExit() {
+        WindowEvent winClosing = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
         Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(winClosing);
     }
 
-    public void getRooms(){
+    public void getRooms() {
         db_connection db = new db_connection();
         Connection conn = null;
         Statement stmt = null;
-        ResultSet resultSet;
+        ResultSet rs;
         try {
-
 //            String sid = staffid.getText();
 //            int id = Integer.parseInt(sid);
             conn = db.connect_db();
             stmt = conn.createStatement();
-            boolean staff,frontDesk,manager;
-            staff = frontDesk = manager = false;
-//            rs = stmt.executeQuery("select staffid from cateringstaff where staffid = " + id);
-            resultSet = stmt.executeQuery("SELECT roomNum, category, capacity FROM Room WHERE avail = 1 AND HotelId = (SELECT HotelId FROM worksFor WHERE StaffId = "+Intermediate.getItem("1")+")");
+            rs = stmt.executeQuery("SELECT roomNum, room.category, capacity, p.price FROM Room, RoomPrice p WHERE avail = 1 AND Room.category = p.category AND HotelId = (SELECT HotelId FROM worksFor WHERE StaffId = " + Intermediate.getItem("1") + ") ORDER BY room.category;");
             Intermediate.removeItem("1");
-            if (resultSet.first()) {
-                ListTableModel model = ListTableModel.createModelFromResultSet(resultSet);
-                JTable table = new JTable( model );
-//                ServiceRecords sr = new ServiceRecords();
-//                sysExit();
-//                sr.setVisible(true);
-//                staff = true;
+            DefaultTableModel model = (DefaultTableModel) Room.getModel();
+            while (rs.next()) {
+                model.addRow(new Object[]{rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)});
             }
 
+            Room.setRowSelectionAllowed(true);
+            ListSelectionModel ls = Room.getSelectionModel();
+            ls.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+            ls.addListSelectionListener(new ListSelectionListener() {
+                public void valueChanged(ListSelectionEvent e) {
+                    String selectedData = null;
+                    int[] selectedRow = Room.getSelectedRows();
+                    for (int i = 0; i < selectedRow.length; i++) {
+                        for (int j = 0; j < 4; j++) {
+                            selectedData = (String) Room.getValueAt(selectedRow[i], j);
+                            System.out.print(selectedData + " ");
+                        }
+                        System.out.println("");
+                    }
+                    //System.out.println("Selected: " + selectedData);
+                }
+            });
+
+            rs = stmt.executeQuery("select custid, name, email from customer");
+            DefaultTableModel modelCust = (DefaultTableModel) Customer.getModel();
+            while (rs.next()) {
+                modelCust.addRow(new Object[]{rs.getString(1), rs.getString(2), rs.getString(3)});
+            }
+            
+            Customer.setRowSelectionAllowed(true);
+            ListSelectionModel lsCust = Customer.getSelectionModel();
+            lsCust.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+            lsCust.addListSelectionListener(new ListSelectionListener() {
+                public void valueChanged(ListSelectionEvent e) {
+                    String selectedData = null;
+                    int[] selectedRow = Customer.getSelectedRows();
+                    for (int i = 0; i < selectedRow.length; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            selectedData = (String) Customer.getValueAt(selectedRow[i], j);
+                            System.out.print(selectedData + " ");
+                        }
+                        System.out.println("");
+                    }
+                    //System.out.println("Selected: " + selectedData);
+                }
+            });
+            
 //            if(staff == false && frontDesk == false && manager == false){
 //                JFrame jf = new JFrame();
 //                JOptionPane.showMessageDialog(jf,"INVALID LOGIN DETAILS","LOGIN ERROR",JOptionPane.ERROR_MESSAGE);
@@ -260,15 +312,15 @@ public class CheckIn extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Confrim;
+    private javax.swing.JTable Customer;
     private javax.swing.JButton Home;
     private javax.swing.JButton Logout;
+    private javax.swing.JTable Room;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JList<String> jList1;
-    private javax.swing.JList<String> jList2;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
     // End of variables declaration//GEN-END:variables
 }
